90dd983f3f1ffa28f6169f7d746f0d16e1b68456
ViktorVan22 <zcg5368@163.com>
2022-07-27 21:15:29 +0800

    add css preprocessor less

--- src/container/Home/index.tsx
+++ src/container/Home/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Button } from "antd-mobile";
+
+import s from "./style.module.less";
+
 const Home = () => {
   return (
-    <div>
+    <div className={s.index}>
       首页
+      <span>样式</span>
       <hr />
       <Button block color="primary" size="large">
         Block Button
