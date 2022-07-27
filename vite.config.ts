54ae63b8e1cd5b59cd05de138f959b4802b13070
ViktorVan22 <zcg5368@163.com>
2022-07-27 21:45:11 +0800

    add postcss-pxtorem

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import postCssPxToRem from 'postcss-pxtorem'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     modules: {
       localsConvention: 'dashesOnly'
+    },
+    postcss: {
+      plugins: [
+        postCssPxToRem({
+          rootValue: 37.5,
+          propList: ['*']
+        })
+      ]
     }
   }
 })
