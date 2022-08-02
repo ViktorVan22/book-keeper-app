16dd756111064e13a52622de432151e4547cdb73
ViktorVan22 <zcg5368@163.com>
2022-08-02 14:53:58 +0800

    update

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestHeaders } from "axios";
 
 const MODE = import.meta.env.MODE  // 环境变量
 
-axios.defaults.baseURL = MODE === 'development' ? 'http://127.0.0.1:7001' : 'http://8.130.17.206'
+axios.defaults.baseURL = MODE === 'development' ? 'http://127.0.0.1:7001' : 'http://8.130.17.206:7001'
 axios.defaults.withCredentials = true
 axios.defaults.headers.head['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['Authorization'] = `${localStorage.getItem('token') || null}`
