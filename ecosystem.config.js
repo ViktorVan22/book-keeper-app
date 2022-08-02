9575f283f5d8a6a82476d6035608a2e942ff325c
ViktorVan22 <zcg5368@163.com>
2022-08-02 16:59:45 +0800

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "book-keeper-app",
+      script: "pm-server.js",
+    },
+  ],
+  deploy: {
+    production: {
+      user: "root",
+      host: "8.130.17.206",
+      ref: "origin/main",
+      repo: "git@github.com:ViktorVan22/book-keeper-app.git",
+      path: "/workspace/book-keeper-app",
+      "post-deploy":
+        "git reset --hard && git checkout main && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js", // -production=false 下载全量包
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  },
+};
